1ceaadeae01329bab3ca43c5f63b87dada582f6e
jonas.silva <jonas.silva@techwayinfo.com.br>
2021-05-15 15:21:14 -0300

    correcao erro

--- src/main/java/com/animalx/AnimalX/config/WebSecurityConfig.java
+++ src/main/java/com/animalx/AnimalX/config/WebSecurityConfig.java
@@ -77,9 +77,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 				.antMatchers(HttpMethod.GET,"/api/animal/animais/adotados").permitAll()
 				.antMatchers("/api/usuario/recuperarSenha/**").permitAll() 
 				.antMatchers("/api/recuperarSenha/**").permitAll() 
-				.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
- 
-				.antMatchers(HttpMethod.GET, "/api/usuario/permitAll/").hasRole("ADMIN")
+				.antMatchers(HttpMethod.GET, "/actuator/**").permitAll() 
+				.antMatchers(HttpMethod.GET, "/api/usuario/usuarios/").hasRole("ADMIN")
 				.antMatchers(HttpMethod.GET, "api/usuario/relatorio").hasRole("ADMIN")
 				  
 				.anyRequest().authenticated();
